78cde9f580284338d5dac79ac60219202bcd1038
Adelakun Ahmad <adelakunahmad@gmail.com>
2025-09-20 20:08:46 -0700

    fixed fundwallet utils build error

--- src/pages/fund-wallet/utils/handleFundWallet.ts
+++ src/pages/fund-wallet/utils/handleFundWallet.ts
@@ -1,38 +1,37 @@
 import { toast } from "sonner";
 import initiateFundWalletTransaction from "../../../shared/utils/initiateWalletTransaction";
-import verifyFundWalletTransaction from "../../../shared/utils/verifyFundWalletTransaction";
 
 export default function handleFundWallet(info: {
-	email: string;
-	amount: number;
+  email: string;
+  amount: number;
 }) {
-	toast.promise(
-		(): Promise<string> =>
-			new Promise((resolve, reject) => {
-				initiateFundWalletTransaction(info)
-					.then(response => {
-						if (!response.status)
-							return reject("We could not initialize the transaction. Try again soon.");
+  toast.promise(
+    (): Promise<string> =>
+      new Promise((resolve, reject) => {
+        initiateFundWalletTransaction(info)
+          .then(response => {
+            if (!response.status)
+              return reject("We could not initialize the transaction. Try again soon.");
 
-						const paymentTab = window.open(
-							response.data.authorization_url,
-							"_blank",
-						);
+            const paymentTab = window.open(
+              response.data.authorization_url,
+              "_blank",
+            );
 
-						if (!paymentTab) reject("Please allow popups for this website");
-						else
-							verifyFundWalletTransaction(response.data.reference, () => {
-								paymentTab.close();
-								resolve("Payment confirmed!");
-							});
-					})
-					.catch(() => reject("An error occurred. Please try again later."));
-
-			}),
-		{
-			loading: "Processing payment. Please wait...",
-			error: (e: string) => e,
-			success: (e: string) => e,
-		},
-	);
+            if (!paymentTab) {
+              reject("Please allow popups for this website");
+            } else {
+              // At this point, Paystack takes over. 
+              // After payment, user will be redirected to your callback route.
+              resolve("Redirecting to payment...");
+            }
+          })
+          .catch(() => reject("An error occurred. Please try again later."));
+      }),
+    {
+      loading: "Processing payment. Please wait...",
+      error: (e: string) => e,
+      success: (e: string) => e,
+    },
+  );
 }
