6f954df79fdfe16621468479e7cd238ad2fb0c08
Adelakun Ahmad <adelakunahmad@gmail.com>
2025-11-17 03:55:49 -0800

    fix laravel echo

--- src/lib/echo.ts
+++ src/lib/echo.ts
@@ -3,9 +3,11 @@ import Echo from "laravel-echo";
 export const echo = new Echo({
     broadcaster: "reverb",
     key: import.meta.env.VITE_REVERB_APP_KEY,
-    wsHost: "backend.hovertask.com",
-    wsPort: 443,
-    wssPort: 443,
+    wsHost: import.meta.env.VITE_REVERB_HOST,
+    wsPort: import.meta.env.VITE_REVERB_PORT,
+    wssPort: import.meta.env.VITE_REVERB_PORT,
     wsPath: "/reverb",
     forceTLS: true,
+    scheme: import.meta.env.VITE_REVERB_SCHEME,
+    enabledTransports: ["ws", "wss"],
 });
